d53273fd5286c907f048d2dfe150de55b74a0409
AgusJems <59831192+AgusJems@users.noreply.github.com>
2025-03-16 15:36:15 +0700

    First Commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,47 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbToastModule, NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CountToModule } from 'angular-count-to';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { SimplebarAngularModule } from 'simplebar-angular';
+
+// Load Icons
+import { defineElement } from 'lord-icon-element';
+import lottie from 'lottie-web';
+
+// Pages Routing
+import { PagesRoutingModule } from "./pages-routing.module";
+import { SharedModule } from "../shared/shared.module";
+import { DashboardComponent } from './dashboards/dashboard/dashboard.component';
+import { ToastsContainer } from './dashboards/dashboard/toasts-container.component';
+import { DashboardsModule } from "./dashboards/dashboards.module";
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ToastsContainer
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbToastModule,
+    FlatpickrModule.forRoot(),
+    CountToModule,
+    NgApexchartsModule,
+    NgbDropdownModule,
+    SimplebarAngularModule,
+    PagesRoutingModule,
+    SharedModule,
+    DashboardsModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class PagesModule {
+  constructor() {
+    defineElement(lottie.loadAnimation);
+  }
+}
